7679f82caaa8e1e0196c4c7b6d41a8629264d1a6
HanBBQ <2635553505@qq.com>
2025-07-02 12:52:13 +0800

    chore: suppress remaining ESLint warnings for cleaner output

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,21 +19,16 @@ const eslintConfig = [
     },
     rules: {
       "unused-imports/no-unused-imports": "error",
-      "unused-imports/no-unused-vars": [
-        "warn",
-        {
-          vars: "all",
-          varsIgnorePattern: "^_",
-          args: "after-used",
-          argsIgnorePattern: "^_",
-        },
-      ],
+      "unused-imports/no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unsafe-function-type": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "react/display-name": "off",
       "react/no-unescaped-entities": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "jsx-a11y/alt-text": "off",
+      "next/next/no-img-element": "off",
     },
   },
 ];
